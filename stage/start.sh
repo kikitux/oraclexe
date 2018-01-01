101d9fc3eea07ee84fde0b673bd909051521a269
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-01-02 00:20:39 +0100

    build and push docker

--- stage/start.sh
+++ stage/start.sh
@@ -9,9 +9,16 @@ export PATH=$ORACLE_HOME/bin:$PATH
 sed -i -E "s/HOST = [^)]+/HOST = ${HOSTNAME}/g" ${ORACLE_HOME}/network/admin/listener.ora
 sed -i -E "s/HOST = [^)]+/HOST = ${HOSTNAME}/g" ${ORACLE_HOME}/network/admin/tnsnames.ora
 service oracle-xe start
-lsnrctl status
+if [ $? -ne 0 ]; then
+  "echo err: oracle-xe service didn't start"
+  "echo remember start container like:"
+  "echo docker run --rm -p 1521:1521 --shm-size=1g --memory=1g --memory-swap=2g <container> ./start.sh"
+  exit 1
+else
+  echo "Oracle started successfully!"
+fi
 
-echo "Oracle started successfully!"
+lsnrctl status
 
 # forever loop just to prevent Docker container to exit, when run as daemon
 while true; do sleep 1000; done
